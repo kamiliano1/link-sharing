9eb7c144645858e087f60fd491189dcd7fdfb1a5
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-01 21:20:50 +0200

    Update PreviewLink.tsx

--- src/app/Layout/Select/PreviewLink.tsx
+++ src/app/Layout/Select/PreviewLink.tsx
@@ -3,11 +3,13 @@ import { ActiveLinksType, PlatfromsType } from "./ActiveLinksType";
 import { linksList } from "./linkList";
 import { IconType } from "react-icons";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import Link from "next/link";
 type PreviewLinkProps = {
   platform: PlatfromsType;
+  link: string;
 };
 
-const PreviewLink: React.FC<PreviewLinkProps> = ({ platform }) => {
+const PreviewLink: React.FC<PreviewLinkProps> = ({ platform, link }) => {
   const [linkStyle, setLinkStyle] = useState<ActiveLinksType>({
     name: "GitHub",
     icon: linksList[0].icon,
@@ -15,15 +17,17 @@ const PreviewLink: React.FC<PreviewLinkProps> = ({ platform }) => {
   });
   useEffect(() => {
     setLinkStyle(linksList.filter((item) => item.name === platform)[0]);
+    console.log(platform);
   }, [platform]);
   return (
-    <div
+    <Link
+      href={link}
       className={`mx-auto w-[237px] p-4 flex text-bodyM items-center rounded-lg ${linkStyle.style}`}
     >
       <linkStyle.icon />
       <p className="ml-3 mr-auto">{linkStyle.name}</p>
       <AiOutlineArrowRight className="text-[1.5rem]" />
-    </div>
+    </Link>
   );
 };
 export default PreviewLink;
