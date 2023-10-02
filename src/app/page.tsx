76c55908bf79c88e8957463122af6c8066af3ea6
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-02 20:33:46 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ import { linksList } from "./Layout/Select/linkList";
 import { useState } from "react";
 import PreviewLink from "./Layout/Select/PreviewLink";
 import { PlatfromsType } from "./Layout/Select/ActiveLinksType";
+import Navbar from "./Layout/Navbar/Navbar";
 type UserLinkss = {
   userLink: {
     platform: PlatfromsType;
@@ -60,7 +61,8 @@ export default function Home() {
     getValues,
     control,
     formState: { errors },
-  } = useForm<UserLinks>({ defaultValues: startingLinks || [] });
+  } = useForm<UserLinks>();
+  // } = useForm<UserLinks>({ defaultValues: startingLinks || [] });
   type UserLinks = {
     userLink: {
       platform: PlatfromsType;
@@ -92,84 +94,128 @@ export default function Home() {
     setGeneratedLinks(data);
   };
   return (
-    <main className="p-[15rem] bg-black h-[100vh]">
-      <form onSubmit={handleSubmit(formSubmit)}>
-        {fields.map((item, index) => (
-          <li key={item.id}>
-            <Controller
-              control={control}
-              name={`userLink.${index}.platform`}
-              defaultValue={"GitHub"}
-              render={({ field: { onChange, value, ref } }) => (
-                <>
-                  <div className="flex justify-between text-grey mb-3">
-                    <h2 className="text-headingS inline-block">
-                      <MdDragHandle className="inline-block mr-2" />
-                      Link #{index + 1}
-                    </h2>
-                    <button type="button" onClick={() => remove(index)}>
-                      Remove
-                    </button>
-                  </div>
-                  <SelectInput onChange={onChange} value={value} ref={ref} />
-                  <p className="text-bodyS mb-1 mt-3 text-darkGrey">Link</p>
-                </>
-              )}
-            />
-            <Controller
-              control={control}
-              name={`userLink.${index}.link`}
-              defaultValue={""}
-              render={({ field }) => (
-                <div className="flex relative w-min">
-                  <Image
-                    src={LinkIcon}
-                    alt="link icon"
-                    className="absolute top-4 left-4"
-                  />
-                  <input
-                    {...field}
-                    type="text"
-                    placeholder={
-                      linksList.find(
-                        (item) => item.name === watch(`userLink.${0}.platform`)
-                      )?.placeholder
-                    }
-                    className={`pl-9 pr-4 py-3 border-[1px] w-[500px] border-borders text-bodyM rounded-lg
-                  text-black 
+    <main className="min-h-[100vh]">
+      <Navbar />
+      <form onSubmit={handleSubmit(formSubmit)} className="flex flex-col">
+        <div className="p-10 sm:p-16 flex flex-col bg-lightGrey">
+          <h1 className="text-headingMmobile mb-2">Customize your links</h1>
+          <p className="text-bodyM text-grey mb-10">
+            Add/edit/remove links below and then share all your profiles with
+            the world!
+          </p>
+          <Button
+            role="secondary"
+            type="button"
+            onClick={() =>
+              append({ platform: "GitHub", link: "", id: nanoid() })
+            }
+          >
+            + Add new link
+          </Button>
+          <div className="min-h-[453px] p-5">
+            {fields.map((item, index) => (
+              <li key={item.id} className="list-none my-5">
+                <Controller
+                  control={control}
+                  name={`userLink.${index}.platform`}
+                  defaultValue={"GitHub"}
+                  render={({ field: { onChange, value, ref } }) => (
+                    <>
+                      <div className="flex justify-between text-grey mb-3">
+                        <h2 className="text-headingS inline-block">
+                          <MdDragHandle className="inline-block mr-2" />
+                          Link #{index + 1}
+                        </h2>
+                        <button type="button" onClick={() => remove(index)}>
+                          Remove
+                        </button>
+                      </div>
+                      <SelectInput
+                        onChange={onChange}
+                        value={value}
+                        ref={ref}
+                      />
+                      <p className="text-bodyS mb-1 mt-3 text-darkGrey">Link</p>
+                    </>
+                  )}
+                />
+                <Controller
+                  control={control}
+                  name={`userLink.${index}.link`}
+                  defaultValue={""}
+                  render={({ field }) => (
+                    <div className="flex relative w-min">
+                      <Image
+                        src={LinkIcon}
+                        alt="link icon"
+                        className="absolute top-4 left-4"
+                      />
+                      <input
+                        {...field}
+                        type="text"
+                        placeholder={
+                          linksList.find(
+                            (item) =>
+                              item.name === watch(`userLink.${index}.platform`)
+                          )?.placeholder
+                        }
+                        className={`pl-9 pr-4 py-3 border-[1px] mb-6 border-borders text-bodyM rounded-lg
+                  text-black
                   hover:shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] hover:border-purple ${
                     errors.userLink?.[index]?.link && "text-red border-red"
                   }`}
-                  />
-                  {errors.userLink?.[index] && (
-                    <>
-                      <p className="text-red text-bodyS absolute top-4 right-4">
-                        {errors.userLink?.[index]?.link?.message}
-                      </p>
-                    </>
+                      />
+                      {errors.userLink?.[index] && (
+                        <>
+                          <p className="text-red text-bodyS absolute top-4 right-4">
+                            {errors.userLink?.[index]?.link?.message}
+                          </p>
+                        </>
+                      )}
+                    </div>
                   )}
-                </div>
-              )}
-            />
-          </li>
-        ))}
-        <Button
-          role="secondary"
-          type="button"
-          onClick={() => append({ platform: "GitHub", link: "", id: nanoid() })}
-        >
-          + Add new link
-        </Button>
-        <Button role="secondary" type="submit">
+                />
+              </li>
+            ))}
+            {!fields.length && (
+              <>
+                <Image
+                  className="mx-auto mt-10 mb-6"
+                  src="/icons/illustration-empty.svg"
+                  width={125}
+                  height={80}
+                  alt="/"
+                />
+                <h2 className="text-headingMmobile text-center">
+                  Let`s get you started
+                </h2>
+                <p className="text-grey mt-6 mb-16">
+                  Use the “Add new link” button to get started. Once you have
+                  more than one link, you can reorder and edit them. We`re here
+                  to help you share your profiles with everyone!
+                </p>
+              </>
+            )}
+          </div>
+          {/* <Button role="secondary" type="submit">
           submit
         </Button>
         <Button role="secondary" type="button" onClick={sprwadz}>
           Sprawdz
+        </Button> */}
+        </div>
+        <span className="w-full h-[1px] mb-4 inline-block bg-borders mx-4"></span>
+        <Button
+          role="primary"
+          disabled={fields.length ? false : true}
+          cssClass="mt-auto mx-8 sm:w-min sm:px-5 sm:self-end"
+        >
+          Save
         </Button>
-      </form>
-      {generatedLinks?.userLink.map((item) => (
+        {/* {generatedLinks?.userLink.map((item) => (
         <PreviewLink key={item.id} platform={item.platform} link={item.link} />
-      ))}
+      ))} */}
+      </form>
     </main>
   );
 }
