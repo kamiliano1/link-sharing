60a24ad4fcaab513701ed637f3d699cf87596bb3
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-04 20:52:05 +0200

    Update Navbar.tsx

--- src/app/Layout/Navbar/Navbar.tsx
+++ src/app/Layout/Navbar/Navbar.tsx
@@ -15,47 +15,11 @@ const Navbar: React.FC<NavbarProps> = () => {
   const pathname = usePathname();
 
   return (
-    <>
-      <div className="py-4 sm:py-10 pl-6 pr-4 sm:pl-12 sm:pr-10 flex items-center justify-between self-stretch">
-        <Link href="/home">
-          <Image src={logoSmall} className="sm:hidden" alt="web logo" />
-          <Image src={logoBig} className="hidden sm:block" alt="web logo" />
-        </Link>
-        <div className="flex gap-[3.375rem]">
-          <Link
-            href="/home"
-            className={`flex items-center hover:text-purple  ${
-              pathname === "/home" ? "text-purple" : "text-grey"
-            } `}
-          >
-            <AiOutlineLink />
-            <p className="ml-2 hidden sm:block">Links</p>
-          </Link>
-
-          <Link
-            href="/profileDetails"
-            className={`flex items-center hover:text-purple ${
-              pathname === "/profileDetails" ? "text-purple" : "text-grey"
-            } `}
-          >
-            <RxAvatar />
-            <p className="ml-2 hidden sm:block">Profile Details</p>
-          </Link>
-        </div>
-
-        <Link
-          href="/previewProfile"
-          className="border-[1px] border-purple rounded-lg text-purple px-4 sm:px-[1.6875rem] py-[0.6875rem]"
-        >
-          <AiOutlineEye className="sm:hidden text-purple" />
-          <p className="hidden sm:block">Preview</p>
-        </Link>
-      </div>
-
-      <NavigationMenu.Root>
-        <NavigationMenu.List>
-          <div className="py-4 sm:py-10 pl-6 pr-4 sm:pl-12 sm:pr-10 flex items-center justify-between self-stretch">
-            <NavigationMenu.Item>
+    <NavigationMenu.Root className=" e">
+      <NavigationMenu.List className="">
+        <div className="py-4 sm:py-10 pl-6 pr-4 sm:pl-12 sm:pr-10 flex items-center justify-between self-stretch">
+          <NavigationMenu.Item>
+            <NavigationMenu.Trigger>
               <Link href="/home">
                 <Image src={logoSmall} className="sm:hidden" alt="web logo" />
                 <Image
@@ -64,10 +28,12 @@ const Navbar: React.FC<NavbarProps> = () => {
                   alt="web logo"
                 />
               </Link>
-            </NavigationMenu.Item>
-            <div className="flex gap-[3.375rem]">
-              {" "}
-              <NavigationMenu.Item>
+            </NavigationMenu.Trigger>
+          </NavigationMenu.Item>
+          <div className="flex gap-[3.375rem]">
+            {" "}
+            <NavigationMenu.Item>
+              <NavigationMenu.Trigger>
                 <Link
                   href="/home"
                   className={`flex items-center hover:text-purple  ${
@@ -77,8 +43,10 @@ const Navbar: React.FC<NavbarProps> = () => {
                   <AiOutlineLink />
                   <p className="ml-2 hidden sm:block">Links</p>
                 </Link>
-              </NavigationMenu.Item>{" "}
-              <NavigationMenu.Item>
+              </NavigationMenu.Trigger>
+            </NavigationMenu.Item>{" "}
+            <NavigationMenu.Item>
+              <NavigationMenu.Trigger>
                 <Link
                   href="/profileDetails"
                   className={`flex items-center hover:text-purple ${
@@ -88,22 +56,23 @@ const Navbar: React.FC<NavbarProps> = () => {
                   <RxAvatar />
                   <p className="ml-2 hidden sm:block">Profile Details</p>
                 </Link>{" "}
-              </NavigationMenu.Item>
-            </div>
-            <NavigationMenu.Item>
-              <Link
-                href="/previewProfile"
-                className="border-[1px] border-purple rounded-lg text-purple
-             px-4 sm:px-[1.6875rem] py-[0.6875rem]"
-              >
+              </NavigationMenu.Trigger>
+            </NavigationMenu.Item>
+          </div>
+          <NavigationMenu.Item>
+            <NavigationMenu.Trigger
+              className="border-[1px] border-purple rounded-lg text-purple
+             px-4 sm:px-[1.6875rem] py-[0.6875rem] flex"
+            >
+              <Link href="/previewProfile">
                 <AiOutlineEye className="sm:hidden text-purple" />
                 <p className="hidden sm:block">Preview</p>
               </Link>
-            </NavigationMenu.Item>
-          </div>
-        </NavigationMenu.List>
-      </NavigationMenu.Root>
-    </>
+            </NavigationMenu.Trigger>
+          </NavigationMenu.Item>
+        </div>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
   );
 };
 export default Navbar;
