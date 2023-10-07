c80455197dee75f048eb53e1ed7844443910a915
kamiliano1 <kamiliano1991@gmail.com>
2023-10-07 13:19:03 +0200

    Update PreviewLink.tsx

--- src/layout/Select/PreviewLink.tsx
+++ src/layout/Select/PreviewLink.tsx
@@ -20,8 +20,7 @@ const PreviewLink: React.FC<PreviewLinkProps> = ({ platform, link }) => {
   return (
     <Link
       href={link}
-      className={`mx-auto w-[237px] p-4 flex text-bodyM items-center rounded-lg mb-5 mr-[.3rem] ${linkStyle.style}`}
-    >
+      className={`mx-auto w-[237px] p-4 flex text-bodyM items-center rounded-lg mb-5 mr-[.3rem] ${linkStyle.style}`}>
       <linkStyle.icon />
       <p className="ml-3 mr-auto">{linkStyle.name}</p>
       <AiOutlineArrowRight className="text-[1.5rem]" />
