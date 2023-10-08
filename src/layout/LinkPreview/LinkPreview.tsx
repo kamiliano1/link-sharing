5f886b72ea274092e83782a914448dff28a6d56a
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-08 21:58:29 +0200

    Update LinkPreview.tsx

--- src/layout/LinkPreview/LinkPreview.tsx
+++ src/layout/LinkPreview/LinkPreview.tsx
@@ -4,21 +4,23 @@ import Image from "next/image";
 import PreviewLink from "../Select/PreviewLink";
 import { useRecoilValue } from "recoil";
 import { previewUserLink } from "@/atoms/previewUserLinkAtom";
+import { userAccountState } from "@/atoms/userAccountAtom";
 type LinkPreviewProps = {};
 
 const LinkPreview: React.FC<LinkPreviewProps> = () => {
   const previewLink = useRecoilValue(previewUserLink);
+  const userAccount = useRecoilValue(userAccountState);
   return (
     <div className="hidden lg:block relative">
       <Image src={PhoneMockup} alt="Phone Mockup" />
       <div className="absolute mt-16 w-full top-0 flex flex-col items-center">
         <div className="aspect-square rounded-full w-[96px] border-[4px] border-purple mb-[1.125rem]"></div>
         <h2 className="text-headingS capitalize">
-          {previewLink.firstName} {previewLink.lastName}
+          {userAccount.firstName} {userAccount.lastName}
         </h2>
-        <p className="text-bodyS mb-20">{previewLink.email}</p>
+        <p className="text-bodyS mb-20">{userAccount.email}</p>
         <div className="max-h-[305px] overflow-y-auto scrollbar">
-          {previewLink?.userLink.map((item) => (
+          {userAccount?.userLink.map((item) => (
             <PreviewLink
               key={item.id}
               platform={item.platform}
