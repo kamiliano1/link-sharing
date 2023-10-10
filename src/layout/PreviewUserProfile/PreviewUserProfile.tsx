0a09779583fb794481cc83f0f4b42b4363c72e4e
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-10 22:25:41 +0200

    Update PreviewUserProfile.tsx

--- src/layout/PreviewUserProfile/PreviewUserProfile.tsx
+++ src/layout/PreviewUserProfile/PreviewUserProfile.tsx
@@ -11,26 +11,23 @@ type PreviewUserProfileProps = {};
 const PreviewUserProfile: React.FC<PreviewUserProfileProps> = () => {
   const previewLink = useRecoilValue(previewUserLink);
   const userAccount = useRecoilValue(userAccountState);
-  const lista: PlatfromsType[] = [
-    "Codepen",
-    "Codewars",
-    "Dev.to",
-    "Frontend Mentor",
-    "GitHub",
-    "Hashnode",
-  ];
   return (
     <div className="mt-16 sm:mt-28 flex flex-col items-center bg-white w-[349px] mx-auto rounded-3xl px-14 py-12">
-      <div className="aspect-square rounded-full w-[96px] border-[4px] border-purple mb-[1.125rem]"></div>
+      {userAccount.picture && (
+        <Image
+          width={96}
+          height={96}
+          src={userAccount.picture}
+          alt="user Avatar"
+          className="aspect-square rounded-full w-[96px] border-[4px] border-purple mb-[1.125rem]"
+        />
+      )}
       <div className="hidden sm:block absolute bg-purple rounded-b-[2rem] top-0 left-0 w-full z-[-1] h-[357px]"></div>
       <h2 className="text-headingM capitalize">
         {userAccount.firstName} {userAccount.lastName}
       </h2>
       <p className="text-headingS mb-14">{userAccount.email} Email</p>
       <div className="max-h-[305px] overflow-y-auto scrollbar">
-        {/* {lista.map((item) => (
-          <PreviewLink key={item} platform={item} link={"item.link"} />
-        ))} */}
         {userAccount.userLink.map((item) => (
           <PreviewLink
             key={item.id}
@@ -38,13 +35,6 @@ const PreviewUserProfile: React.FC<PreviewUserProfileProps> = () => {
             link={item.link}
           />
         ))}
-        {/* {previewLink?.userLink.map((item) => (
-          <PreviewLink
-            key={item.id}
-            platform={item.platform}
-            link={item.link}
-          />
-        ))} */}
       </div>
     </div>
   );
