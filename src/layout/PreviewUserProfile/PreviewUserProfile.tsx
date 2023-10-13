717e61e4034e32404655cb2a30c95754f545a038
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-13 21:55:11 +0200

    Update PreviewUserProfile.tsx

--- src/layout/PreviewUserProfile/PreviewUserProfile.tsx
+++ src/layout/PreviewUserProfile/PreviewUserProfile.tsx
@@ -12,44 +12,46 @@ const PreviewUserProfile: React.FC<PreviewUserProfileProps> = () => {
   const previewLink = useRecoilValue(previewUserLink);
   const userAccount = useRecoilValue(userAccountState);
   return (
-    <div className="mt-16 sm:mt-28 flex flex-col items-center bg-white w-[349px] mx-auto rounded-3xl px-14 py-12">
-      {userAccount.picture ? (
-        <Image
-          width={96}
-          height={96}
-          src={userAccount.picture}
-          alt="user Avatar"
-          className="aspect-square rounded-full w-[96px] border-[4px] border-purple mb-[1.35rem]"
-        />
-      ) : (
-        <div className="w-[96px] aspect-square mb-[1.35rem]"></div>
-      )}
-      <div className="hidden sm:block absolute bg-purple rounded-b-[2rem] top-0 left-0 w-full z-[-1] h-[357px]"></div>
-
-      <h2
-        className={`text-headingM capitalize w-[250px] truncate text-center  ${
-          userAccount.firstName && "bg-white"
-        } `}
-      >
-        {userAccount.firstName} {userAccount.lastName}
-      </h2>
-      <p
-        className={`text-headingS mb-[50px] w-[250px] truncate text-center ${
-          userAccount.email && "bg-white"
-        }`}
-      >
-        {userAccount.email}
-      </p>
-      <div className="max-h-[305px] overflow-y-auto scrollbar">
-        {userAccount.userLink.map((item) => (
-          <PreviewLink
-            key={item.id}
-            platform={item.platform}
-            link={item.link}
+    <>
+      <div className="mt-16 sm:mt-28 flex flex-col items-center bg-white w-[349px] mx-auto rounded-3xl px-14 py-12 min-h-[569px] shadow-[0px_0px_32px_0px_rgba(0,_0,_0,_0.10)] z-[5] relative">
+        {userAccount.picture ? (
+          <Image
+            width={96}
+            height={96}
+            src={userAccount.picture}
+            alt="user Avatar"
+            className="aspect-square rounded-full w-[96px] border-[4px] border-purple mb-[1.35rem]"
           />
-        ))}
+        ) : (
+          <div className="w-[96px] aspect-square mb-[1.35rem]"></div>
+        )}
+        <h2
+          className={`text-headingM capitalize w-[250px] truncate text-center  ${
+            userAccount.firstName && "bg-white"
+          } `}
+        >
+          {userAccount.firstName} {userAccount.lastName}
+        </h2>
+        <p
+          className={`text-headingS mb-[50px] w-[250px] truncate text-center ${
+            userAccount.email && "bg-white"
+          }`}
+        >
+          {userAccount.email}
+        </p>
+        <div className="max-h-[305px] overflow-y-auto scrollbar">
+          {userAccount.userLink.map((item) => (
+            <PreviewLink
+              id={item.id}
+              key={item.id}
+              platform={item.platform}
+              link={item.link}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="hidden sm:block absolute bg-purple rounded-b-[2rem] -top-6 left-0 w-full h-[357px]"></div>
+    </>
   );
 };
 export default PreviewUserProfile;
