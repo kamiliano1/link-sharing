9818ee32b0c6a43dff356235df573f6ac6335c5c
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-15 22:34:21 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import CustomizeUserLinks from "@/layout/CustomizeUserLinks/CustomizeUserLinks";
 import LinkPreview from "@/layout/LinkPreview/LinkPreview";
+import Modal from "@/layout/Modal/Modal";
 import Navbar from "@/layout/Navbar/Navbar";
 import PopUp from "@/layout/PopUp/PopUp";
 
 export default function Home() {
   return (
     <main className="min-h-[100vh]">
+      <Modal />
       <Navbar />
       <div className="lg:flex relative pb-5 lg:justify-center">
         <LinkPreview />
