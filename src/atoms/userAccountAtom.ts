23bea5dc5c3ec91693746b7a19b3af3decfc0394
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-17 22:24:54 +0200

    Update userAccountAtom.ts

--- src/atoms/userAccountAtom.ts
+++ src/atoms/userAccountAtom.ts
@@ -1,16 +1,18 @@
 import { PlatfromsType } from "@/layout/Select/ActiveLinksType";
 import { atom } from "recoil";
 
+export type UserLink = {
+  platform: PlatfromsType;
+  link: string;
+  id: string;
+};
+
 export type UserAccountState = {
   firstName?: string;
   lastName?: string;
   email?: string;
   picture?: string;
-  userLink: {
-    platform: PlatfromsType;
-    link: string;
-    id: string;
-  }[];
+  userLink: UserLink[];
 };
 
 const defaultUserAccountState: UserAccountState = {
