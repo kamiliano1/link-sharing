a504d59ac706606ccc211c9d5988e0bea8f83be6
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-21 11:31:03 +0200

    Update Navbar.tsx

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -3,20 +3,26 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import {} from "react-firebase-hooks/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { AiOutlineEye, AiOutlineLink } from "react-icons/ai";
 import { RxAvatar } from "react-icons/rx";
 import logoBig from "../../../public/icons/logo-devlinks-large.svg";
 import logoSmall from "../../../public/icons/logo-devlinks-small.svg";
+import { auth } from "@/app/firebase/clientApp";
+import { signOut } from "firebase/auth";
 
 type NavbarProps = {};
-
 const Navbar: React.FC<NavbarProps> = () => {
+  const [user] = useAuthState(auth);
   const pathname = usePathname();
+  const logout = () => {
+    signOut(auth);
+  };
+  const urlToCopy = location.href;
   return (
     <NavigationMenu.Root>
       <NavigationMenu.List>
-        {pathname === "/previewProfile" ? (
+        {pathname === `/previewProfile/${user?.uid}` ? (
           <div className="bg-white sm:m-6 px-6 py-4 sm:rounded-xl flex justify-between gap-4 items-center self-stretch relative z-[5]">
             <NavigationMenu.Item className="w-full max-w-[159px]">
               <NavigationMenu.Trigger className="w-full border-[1px] text-purple border-purple bg-white enabled:hover:bg-lightPurple disabled:opacity-25 text-headingS flex justify-center py-[0.6875rem] rounded-lg">
@@ -26,7 +32,12 @@ const Navbar: React.FC<NavbarProps> = () => {
               </NavigationMenu.Trigger>
             </NavigationMenu.Item>
             <NavigationMenu.Item className="w-full max-w-[159px]">
-              <NavigationMenu.Trigger className="bg-purple text-white enabled:hover:bg-purpleHover disabled:opacity-25 shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] w-full text-headingS flex justify-center py-[0.6875rem] rounded-lg">
+              <NavigationMenu.Trigger
+                onClick={() => {
+                  navigator.clipboard.writeText(urlToCopy);
+                }}
+                className="bg-purple text-white enabled:hover:bg-purpleHover disabled:opacity-25 shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] w-full text-headingS flex justify-center py-[0.6875rem] rounded-lg"
+              >
                 Share Link
               </NavigationMenu.Trigger>
             </NavigationMenu.Item>
@@ -81,7 +92,7 @@ const Navbar: React.FC<NavbarProps> = () => {
             <NavigationMenu.Item>
               <NavigationMenu.Trigger className="">
                 <Link
-                  href="/previewProfile"
+                  href={`/previewProfile/${user?.uid}`}
                   className="text-purple border-[1px] px-4 py-3 border-purple bg-white hover:bg-lightPurple w-full text-headingS flex justify-center rounded-lg"
                 >
                   <AiOutlineEye className="sm:hidden" />
@@ -91,6 +102,12 @@ const Navbar: React.FC<NavbarProps> = () => {
             </NavigationMenu.Item>
           </div>
         )}
+        <button
+          className="z-[999999] p-5 bg-darkGrey text-white"
+          onClick={logout}
+        >
+          Logout
+        </button>
       </NavigationMenu.List>
     </NavigationMenu.Root>
   );
