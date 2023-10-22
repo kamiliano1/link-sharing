a37d4e3ff449965457c78aba8dbcbc4ebe3b9af8
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-10-22 21:18:46 +0200

    Update CustomizeUserAccount.tsx

--- src/layout/CustomizeUserAccount/CustomizeUserAccount.tsx
+++ src/layout/CustomizeUserAccount/CustomizeUserAccount.tsx
@@ -9,7 +9,7 @@ import { popUpState } from "@/atoms/togglePopUpAtom";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
 import { useAuthState } from "react-firebase-hooks/auth";
-import useDataFromFirebase from "../../utility/useDataFromFirebase";
+import CustomizeUserAccountSkeleton from "../Skeletons/CustomizeUserAccountSkeleton";
 type CustomizeUserAccountProps = {};
 
 const CustomizeUserAccount: React.FC<CustomizeUserAccountProps> = () => {
@@ -27,10 +27,14 @@ const CustomizeUserAccount: React.FC<CustomizeUserAccountProps> = () => {
     watch,
     setValue,
     getValues,
+    reset,
     control,
     formState: { errors },
   } = useForm<UserAccountState>();
   const selectedFileRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    reset();
+  }, [reset, userAccount]);
   const formSubmit: SubmitHandler<UserAccountState> = async (data) => {
     setIsLoading(true);
     const userLinkRef = doc(firestore, `users/${user?.uid}`);
@@ -61,6 +65,9 @@ const CustomizeUserAccount: React.FC<CustomizeUserAccountProps> = () => {
     setIsPopUpOpen({ togglePopUp: true });
   };
   useEffect(() => {
+    setPictureURL(userAccount.picture || "");
+  }, [userAccount.picture]);
+  useEffect(() => {
     setIsPopUpOpen({ togglePopUp: false });
   }, [setIsPopUpOpen]);
   const onSelectAvatar = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -89,97 +96,105 @@ const CustomizeUserAccount: React.FC<CustomizeUserAccountProps> = () => {
         <h1 className="text-headingMmobile sm:text-headingM mb-2">
           Profile Details
         </h1>
+
         <p className="text-bodyM text-grey mb-10">
           Add your details to create a personal touch to your profile.
         </p>
-        <div className="sm:flex items-center bg-lightGrey rounded-xl p-5">
-          <p className="text-bodyM text-grey mb-10 sm:mb-0 sm:w-[255px]">
-            Profile picture.
-          </p>
-          <input
-            type="file"
-            onChange={onSelectAvatar}
-            ref={selectedFileRef}
-            hidden
-            accept=".png,.jpg"
-          />
-          <div
-            style={{
-              backgroundImage: `url(${pictureURL})`,
-            }}
-            className={`text-purple bg-lightPurple mb-6 sm:mb-0 flex flex-col items-center text-headingS bg-cover bg-no-repeat w-min px-10 py-[3.75rem] cursor-pointer relative rounded-xl sm:w-[193px] ${
-              pictureURL && "hover:text-white hover:bg-grey bg-blend-multiply "
-            }`}
-            onClick={() => selectedFileRef.current?.click()}
-          >
-            <LiaImageSolid className="text-[2.5rem] " />
-            <p className="w-[116px]">+ Upload Image</p>
-          </div>
-          <p className="text-bodyXS text-grey sm:max-w-[127px] sm:ml-6">
-            Image must be below 1024x1024px. Use PNG or JPG format.
-          </p>
-        </div>
-        <div className="mt-6 sm:flex sm:flex-col sm:gap-3 bg-lightGrey rounded-xl p-5">
-          <div className="relative sm:flex sm:gap-4 sm:items-center">
-            <label htmlFor="firstName">
-              <p className="text-bodyXS sm:text-bodyM cursor-pointer text-grey mb-1 sm:mb-0 sm:w-[240px]">
-                First name*
+        {loading ? (
+          <CustomizeUserAccountSkeleton />
+        ) : (
+          <>
+            <div className="sm:flex items-center bg-lightGrey rounded-xl p-5">
+              <p className="text-bodyM text-grey mb-4 sm:mb-0 sm:w-[255px]">
+                Profile picture.
               </p>
-            </label>
-            <input
-              {...register("firstName", {
-                required: "Can`t be empty",
-              })}
-              id="firstName"
-              type="text"
-              defaultValue={userAccount.firstName}
-              placeholder="e.g. John"
-              className={`px-4 w-full py-3 border-[1px] mb-3 sm:mb-0 border-borders max-w-[668px] text-bodyM rounded-lg text-black hover:shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] hover:border-purple ${
-                errors.firstName && "text-red border-red"
-              }`}
-            />
-            <p className="text-red text-bodyXS absolute top-[2.4rem] sm:top-4 right-4">
-              {errors.firstName?.message}
-            </p>
-          </div>
-          <div className="relative sm:flex sm:gap-4 sm:items-center">
-            <label htmlFor="lastName">
-              <p className="text-bodyXS sm:text-bodyM cursor-pointer text-grey mb-1 sm:mb-0 sm:w-[240px]">
-                Last name*
+              <input
+                type="file"
+                onChange={onSelectAvatar}
+                ref={selectedFileRef}
+                hidden
+                accept=".png,.jpg"
+              />
+              <div
+                style={{
+                  backgroundImage: `url(${pictureURL})`,
+                }}
+                className={`text-purple bg-lightPurple mb-6 sm:mb-0 flex flex-col items-center text-headingS bg-cover bg-no-repeat w-min px-10 py-[3.75rem] cursor-pointer relative rounded-xl sm:w-[193px] ${
+                  pictureURL &&
+                  "hover:text-white hover:bg-grey bg-blend-multiply "
+                }`}
+                onClick={() => selectedFileRef.current?.click()}
+              >
+                <LiaImageSolid className="text-[2.5rem] " />
+                <p className="w-[116px]">+ Upload Image</p>
+              </div>
+              <p className="text-bodyXS text-grey sm:max-w-[127px] sm:ml-6">
+                Image must be below 1024x1024px. Use PNG or JPG format.
               </p>
-            </label>
-            <input
-              {...register("lastName", {
-                required: "Can`t be empty",
-              })}
-              id="lastName"
-              type="text"
-              defaultValue={userAccount.lastName}
-              placeholder="e.g. Appleseed"
-              className={`px-4 w-full py-3 border-[1px] mb-3 sm:mb-0 border-borders max-w-[668px] text-bodyM rounded-lg text-black hover:shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] hover:border-purple ${
-                errors.lastName && "text-red border-red"
-              }`}
-            />
-            <p className="text-red text-bodyXS absolute top-[2.4rem] sm:top-4 right-4">
-              {errors.lastName?.message}
-            </p>
-          </div>
-          <div className="relative sm:flex sm:gap-4 sm:items-center">
-            <label htmlFor="email">
-              <p className="text-bodyXS sm:text-bodyM cursor-pointer text-grey mb-1 sm:mb-0 sm:w-[240px]">
-                Email
-              </p>
-            </label>
-            <input
-              {...register("email", {})}
-              id="email"
-              type="text"
-              defaultValue={userAccount.email}
-              placeholder="e.g. email@example.com"
-              className="px-4 w-full py-3 border-[1px] sm:mb-0 border-borders max-w-[668px] text-bodyM rounded-lg text-black hover:shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] hover:border-purple"
-            />
-          </div>
-        </div>
+            </div>
+            <div className="mt-6 sm:flex sm:flex-col sm:gap-3 bg-lightGrey rounded-xl p-5">
+              <div className="relative sm:flex sm:gap-4 sm:items-center">
+                <label htmlFor="firstName">
+                  <p className="text-bodyXS sm:text-bodyM cursor-pointer text-grey mb-1 sm:mb-0 sm:w-[240px]">
+                    First name*
+                  </p>
+                </label>
+                <input
+                  {...register("firstName", {
+                    required: "Can`t be empty",
+                  })}
+                  id="firstName"
+                  type="text"
+                  defaultValue={userAccount.firstName}
+                  placeholder="e.g. John"
+                  className={`px-4 w-full py-3 border-[1px] mb-3 sm:mb-0 border-borders max-w-[668px] text-bodyM rounded-lg text-black hover:shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] hover:border-purple ${
+                    errors.firstName && "text-red border-red"
+                  }`}
+                />
+                <p className="text-red text-bodyXS absolute top-[2.4rem] sm:top-4 right-4">
+                  {errors.firstName?.message}
+                </p>
+              </div>
+              <div className="relative sm:flex sm:gap-4 sm:items-center">
+                <label htmlFor="lastName">
+                  <p className="text-bodyXS sm:text-bodyM cursor-pointer text-grey mb-1 sm:mb-0 sm:w-[240px]">
+                    Last name*
+                  </p>
+                </label>
+                <input
+                  {...register("lastName", {
+                    required: "Can`t be empty",
+                  })}
+                  id="lastName"
+                  type="text"
+                  defaultValue={userAccount.lastName}
+                  placeholder="e.g. Appleseed"
+                  className={`px-4 w-full py-3 border-[1px] mb-3 sm:mb-0 border-borders max-w-[668px] text-bodyM rounded-lg text-black hover:shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] hover:border-purple ${
+                    errors.lastName && "text-red border-red"
+                  }`}
+                />
+                <p className="text-red text-bodyXS absolute top-[2.4rem] sm:top-4 right-4">
+                  {errors.lastName?.message}
+                </p>
+              </div>
+              <div className="relative sm:flex sm:gap-4 sm:items-center">
+                <label htmlFor="email">
+                  <p className="text-bodyXS sm:text-bodyM cursor-pointer text-grey mb-1 sm:mb-0 sm:w-[240px]">
+                    Email
+                  </p>
+                </label>
+                <input
+                  {...register("email", {})}
+                  id="email"
+                  type="text"
+                  defaultValue={userAccount.email}
+                  placeholder="e.g. email@example.com"
+                  className="px-4 w-full py-3 border-[1px] sm:mb-0 border-borders max-w-[668px] text-bodyM rounded-lg text-black hover:shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] hover:border-purple"
+                />
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <span className="h-[1px] inline-block bg-borders mx-4 sm:mx-6"></span>
       <div className="bg-white m-4 sm:m-6 sm:mt-0 mt-0 p-4">
