96c008127f2aa051fa9c12f1f41e6c819b5ca884
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-11-01 10:41:05 +0100

    Replace Link to:/profileDetails with router.back()

--- src/layout/Navbar/PreviewUserNavbar.tsx
+++ src/layout/Navbar/PreviewUserNavbar.tsx
@@ -4,12 +4,13 @@ import Link from "next/link";
 import { User } from "firebase/auth";
 import { popUpState } from "@/atoms/togglePopUpAtom";
 import { useRecoilState } from "recoil";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 type PreviewUserNavbarProps = { user: User | null | undefined };
 
 const PreviewUserNavbar: React.FC<PreviewUserNavbarProps> = ({ user }) => {
   const [isPopUpOpen, setIsPopUpOpen] = useRecoilState(popUpState);
   const pathname = usePathname();
+  const router = useRouter();
   const copyLinkToClipBoard = () => {
     navigator.clipboard.writeText(
       `https://link-sharing-iota.vercel.app${pathname}`
@@ -19,20 +20,21 @@ const PreviewUserNavbar: React.FC<PreviewUserNavbarProps> = ({ user }) => {
   return (
     <NavigationMenu.Root>
       <NavigationMenu.List className="bg-white sm:m-6 px-6 py-4 sm:rounded-xl flex justify-between gap-4 items-center relative z-[5]">
-        <NavigationMenu.Item className="">
-          <Link href="/profileDetails" passHref legacyBehavior>
-            <NavigationMenu.Link className="flex px-7 border-[1px] text-purple border-purple bg-white hover:bg-lightPurple text-headingS py-[0.6875rem] rounded-lg">
-              Back to Editor
-            </NavigationMenu.Link>
-          </Link>
+        <NavigationMenu.Item
+          className="cursor-pointer"
+          onClick={() => router.back()}
+        >
+          <NavigationMenu.Trigger className="flex px-7 border-[1px] text-purple border-purple bg-white hover:bg-lightPurple text-headingS py-[0.6875rem] rounded-lg">
+            Back to Editor
+          </NavigationMenu.Trigger>
         </NavigationMenu.Item>
-        <NavigationMenu.Item className="">
-          <NavigationMenu.Link
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger
             onClick={copyLinkToClipBoard}
             className="flex px-7 bg-purple text-white hover:bg-purpleHover cursor-pointer shadow-[0px_0px_32px_0px_rgba(99,_60,_255,_0.25)] w-full text-headingS justify-center py-[0.6875rem] rounded-lg"
           >
             Share Link
-          </NavigationMenu.Link>
+          </NavigationMenu.Trigger>
         </NavigationMenu.Item>
       </NavigationMenu.List>
     </NavigationMenu.Root>
